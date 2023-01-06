c44d990cb6d64ad4f1c5f8e56c2f3c16651adb9c
Dan B <mrpickles3rd@googlemail.com>
2023-01-06 15:43:19 +0000

    day 7 p 1 & 2

---
+++ 7.js
@@ -0,0 +1,132 @@
+const get = require('lodash/get');
+const set = require('lodash/set');
+
+const { input7 } = require('./7.input');
+
+const BASE_OBJ = { size: 0 };
+
+function dir(fileSystem, pwd, dir) {
+    const thisDir = get(fileSystem, [...pwd, dir]);
+    if (thisDir === undefined) {
+        set(fileSystem, [...pwd, dir], { ...BASE_OBJ });
+        return;
+    }
+}
+
+function cd(fileSystem, pwd, arg, sizes) {
+    switch (arg) {
+        case '/':
+            // Root
+            break;
+        case '..':
+            const dir = pwd.pop();
+            const s1 = get(fileSystem, [...pwd, 'size'], 0);
+            const s2 = get(fileSystem, [...pwd, dir, 'size'], 0);
+
+            set(fileSystem, [...pwd, 'size'], s1 + s2);
+            sizes.set([...pwd].join('.'), get(fileSystem, [...pwd, 'size']));
+
+            break;
+        default:
+            pwd.push(arg);
+
+            break;
+    }
+}
+
+function ls(fileSystem, pwd) {}
+
+const run = { cd, ls };
+
+function main(input) {
+    const sizes = new Map();
+    const pwd = ['/'];
+    const fileSystem = { '/': { ...BASE_OBJ } };
+
+    input.split('\n').forEach((val, idx) => {
+        if (val.indexOf('$') === 0) {
+            const [_, cmd, arg] = val.split(' ');
+            run[cmd](fileSystem, pwd, arg, sizes);
+            return;
+        }
+
+        const [first, second] = val.split(' ');
+        if (first === 'dir') {
+            dir(fileSystem, pwd, second);
+            return;
+        }
+
+        const size = parseInt(first, 10);
+        if (isNaN(size)) {
+            throw `OPPS! Bad Things Happened :(
+"first" is not a number
+typeof first is ${typeof first} - "${first}" - ${idx}
+???${val}???`
+        }
+
+        set(
+            fileSystem,
+            [...pwd, 'size'],
+            get(fileSystem, [...pwd, 'size']) + size,
+        );
+
+        sizes.set([...pwd].join('.'), get(fileSystem, [...pwd, 'size']))
+    }, 0);
+
+    while (pwd.length > 0) {
+        run.cd(fileSystem, pwd, '..', sizes);
+    }
+
+    return sizes;
+}
+
+function day7(input = input7) {
+    const sizes = main(input);
+
+    let ret = 0;
+    for ([file, value] of sizes) {
+        if (value <= 100000) {
+            ret += value;
+        }
+    }
+
+    return ret;
+}
+/*
+let disk = 70_000_000;
+let needed = 30_000_000;
+let root = sizes.get(&PathBuf::from("/")).unwrap();
+let available = disk - root;
+
+sizes
+    .into_values()
+    .filter(|size| available + size >= needed)
+*/
+function day7p7(input = input7) {
+    const sizes = main(input);
+    const disk = 70000000;
+    const needed = 30000000;
+    const root = sizes.get('/');
+    const available = disk - root;
+    const files = [];
+    const WTF = [];
+
+    for ([key, val] of sizes) {
+        WTF.push([key, val])
+        if (available + val >= needed) {
+            console.log("DB ... 🚀 ~ file: 7.js:105 ~ day7p7 ~ key, val", key, val)
+            files.push([key, val]);
+        }
+    };
+
+        const [_, ret] = files.sort(
+        ([_1, a], [_2, b]) => a>b ? -1 : a<b ? 1 : 0,
+    ).pop();
+
+    return ret;
+}
+
+module.exports = {
+    day7,
+    day7p7,
+};
---
+++ 7.test.js
@@ -0,0 +1,62 @@
+const { day7, day7p7 } = require('./7');
+const { input7 } = require('./7.input');
+
+/*
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+
+move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+ */
+
+describe('day7', () => {
+    describe('setup', () => {
+        const inp = `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`;
+
+        it('Should TEMP, ret the wrong item', () => {
+            expect(day7(inp)).toEqual(95437);//95437
+        });
+
+        it('Should do a thing', () => {
+            // expect(day5()).toBeLessThan(548);
+            expect(day7()).toEqual(1454188);
+        });
+
+        it.only('Should do a thing 2', () => {
+            expect(day7p7(inp)).toBe(24933642);
+
+            expect(day7p7()).toBeLessThan(15711259);
+            expect(day7p7()).toBeLessThan(5406295);
+
+            expect(day7p7()).toBeGreaterThan(133507);
+
+            expect(day7p7()).toBe(4183246);
+        });
+    });
+});
