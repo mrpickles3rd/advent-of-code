20e1e943189470e2a61329d61c8c6a6ba67705d5
Dan B <mrpickles3rd@googlemail.com>
2023-01-10 09:10:39 +0000

    day 9 test input

--- 9.test.js
+++ 9.test.js
@@ -3,7 +3,14 @@ const { input9 } = require('./9.input');
 
 describe('day9', () => {
     describe('setup', () => {
-        const inp = ``;
+        const inp = `R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2`;
 
         it('Should do one thing', () => {
             expect(day9(inp)).toEqual(123);
