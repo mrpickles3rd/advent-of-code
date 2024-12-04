9b7c7fb0fd5f51f47bed9e294513e7612566be1a
Dan B <mrpickles3rd@googlemail.com>
2024-12-04 09:26:39 +0000

    Day 4 test 1 passing

---
+++ 2024/day4.mjs
@@ -0,0 +1,57 @@
+var testInput = `..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X....`;
+
+var testWithAllLetters = `MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX`;
+
+function countXMAS(input) {
+    const rows = input.split('\n');
+    const word = 'XMAS';
+    const wordLength = word.length;
+    let count = 0;
+
+    const directions = [
+        { x: 0, y: 1 },   // horizontal right
+        { x: 1, y: 0 },   // vertical down
+        { x: 1, y: 1 },   // diagonal down-right
+        { x: 1, y: -1 },  // diagonal down-left
+        { x: 0, y: -1 },  // horizontal left
+        { x: -1, y: 0 },  // vertical up
+        { x: -1, y: -1 }, // diagonal up-left
+        { x: -1, y: 1 }   // diagonal up-right
+    ];
+
+    for (let i = 0; i < rows.length; i++) {
+        for (let j = 0; j < rows[i].length; j++) {
+            for (const direction of directions) {
+                let found = true;
+                for (let k = 0; k < wordLength; k++) {
+                    const x = i + k * direction.x;
+                    const y = j + k * direction.y;
+                    if (x < 0 || x >= rows.length || y < 0 || y >= rows[i].length || rows[x][y] !== word[k]) {
+                        found = false;
+                        break;
+                    }
+                }
+                if (found) {
+                    count++;
+                }
+            }
+        }
+    }
+
+    return count;
+}
+
+console.log(countXMAS(testInput)); // Output the count of "XMAS"
