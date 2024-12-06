65946d5938f72236cbba53f46159cb4fe8419136
Dan B <mrpickles3rd@googlemail.com>
2024-12-06 08:25:17 +0000

    Done with no help, just the chalange put in the prompt.

---
+++ 2024/day6.mjs
@@ -0,0 +1,59 @@
+import { input } from './day6.input.mjs';
+
+var testInput = `....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...`;
+
+function predictGuardPath(map) {
+    const directions = ['^', '>', 'v', '<'];
+    const moves = {
+        '^': [-1, 0],
+        '>': [0, 1],
+        'v': [1, 0],
+        '<': [0, -1]
+    };
+    let guardPos = [0, 0];
+    let guardDir = '^';
+    const visited = new Set();
+
+    // Find initial guard position and direction
+    for (let r = 0; r < map.length; r++) {
+        for (let c = 0; c < map[r].length; c++) {
+            if (directions.includes(map[r][c])) {
+                guardPos = [r, c];
+                guardDir = map[r][c];
+                break;
+            }
+        }
+    }
+
+    while (true) {
+        visited.add(guardPos.join(','));
+        const [dr, dc] = moves[guardDir];
+        const [nr, nc] = [guardPos[0] + dr, guardPos[1] + dc];
+
+        if (nr < 0 || nr >= map.length || nc < 0 || nc >= map[0].length) {
+            break;
+        }
+
+        if (map[nr][nc] === '#') {
+            guardDir = directions[(directions.indexOf(guardDir) + 1) % 4];
+        } else {
+            guardPos = [nr, nc];
+        }
+    }
+
+    return visited.size;
+}
+
+// const map = testInput.split('\n').map(line => line.split(''));
+const map = input.split('\n').map(line => line.split(''));
+console.log(predictGuardPath(map));
+
