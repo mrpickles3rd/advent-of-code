243dec9f5292c96fc0d09c3ed5ee98b9a2cfc906
Dan B <mrpickles3rd@googlemail.com>
2024-12-09 11:53:27 +0000

    done

--- 2024/day6.p2.mjs
+++ 2024/day6.p2.mjs
@@ -36,7 +36,6 @@ function predictGuardPath(map) {
 
     while (true) {
         visited.add(guardPos.join(','));
-        map[guardPos[0]][guardPos[1]] = 'X';
         const [dr, dc] = moves[guardDir];
         const [nr, nc] = [guardPos[0] + dr, guardPos[1] + dc];
 
@@ -44,14 +43,74 @@ function predictGuardPath(map) {
             break;
         }
 
-        if (map[nr][nc] === '#') {
+        if (map[nr][nc] === '#' || map[nr][nc] === 'O') {
             guardDir = directions[(directions.indexOf(guardDir) + 1) % 4];
         } else {
             guardPos = [nr, nc];
+            map[guardPos[0]][guardPos[1]] = 'X';
         }
     }
 
-    return { map, visitedCount: visited.size };
+    return { map, visitedCount: visited.size, guardPos, guardDir };
+}
+
+function countLoops(map) {
+    const directions = ['^', '>', 'v', '<'];
+    const moves = {
+        '^': [-1, 0],
+        '>': [0, 1],
+        'v': [1, 0],
+        '<': [0, -1]
+    };
+    const { map: resultMap, visitedCount, guardPos, guardDir } = predictGuardPath(map);
+    let loopCount = 0;
+
+    for (let r = 0; r < resultMap.length; r++) {
+        for (let c = 0; c < resultMap[r].length; c++) {
+            if (resultMap[r][c] === 'X') {
+                const newMap = resultMap.map(row => row.slice());
+                newMap[r][c] = 'O';
+                const { visitedCount: newVisitedCount, guardPos: newGuardPos, guardDir: newGuardDir } = predictGuardPath(newMap);
+
+                if (newVisitedCount < visitedCount) {
+                    loopCount++;
+                } else {
+                    // Check if the guard gets stuck in a loop
+                    const loopVisited = new Set();
+                    let stuck = false;
+                    while (true) {
+                        loopVisited.add(newGuardPos.join(','));
+                        const [dr, dc] = moves[newGuardDir];
+                        const [nr, nc] = [newGuardPos[0] + dr, newGuardPos[1] + dc];
+
+                        if (nr < 0 || nr >= newMap.length || nc < 0 || nc >= newMap[0].length || newMap[nr][nc] === 'O') {
+                            break;
+                        }
+
+                        if (newMap[nr][nc] === '#' || newMap[nr][nc] === 'O') {
+                            newGuardDir = directions[(directions.indexOf(newGuardDir) + 1) % 4];
+                        } else {
+                            newGuardPos = [nr, nc];
+                        }
+
+                        if (loopVisited.has(newGuardPos.join(','))) {
+                            stuck = true;
+                            break;
+                        }
+                    }
+
+                    if (stuck) {
+                        loopCount++;
+                        console.log(newMap.map(line => line.join('')).join('\n'));
+                        console.log('---');
+                    }
+                }
+            }
+        }
+    }
+
+    console.log(resultMap.map(line => line.join('')).join('\n'));
+    return loopCount;
 }
 
 // const map = testInput.split('\n').map(line => line.split(''));
@@ -59,3 +118,4 @@ const map = testInput.split('\n').map(line => line.split(''));
 const { map: resultMap, visitedCount } = predictGuardPath(map);
 console.log(resultMap.map(line => line.join('')).join('\n'));
 console.log(`Distinct positions visited: ${visitedCount}`);
+console.log(`Number of loops: ${countLoops(map)}`);
---
+++ 2024/day7.mjs
@@ -0,0 +1,50 @@
+import { input } from "./day7.input.mjs";
+
+var testInput = `190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20`;
+
+function evaluateEquation(testValue, numbers) {
+    const operators = ['+', '*'];
+    const n = numbers.length;
+    const stack = [[numbers[0], 1]]; // [current value, index]
+
+    while (stack.length) {
+        const [current, index] = stack.pop();
+        if (index === n) {
+            if (current === testValue) return true;
+            continue;
+        }
+        for (const op of operators) {
+            const nextValue = op === '+' ? current + numbers[index] : current * numbers[index];
+            stack.push([nextValue, index + 1]);
+        }
+    }
+    return false;
+}
+
+function sumOfValidTestValues(input) {
+    const lines = input.trim().split('\n');
+    let sum = 0;
+
+    for (const line of lines) {
+        const [testValue, numbers] = line.split(': ');
+        const testValueNum = parseInt(testValue);
+        const numberArray = numbers.split(' ').map(Number);
+
+        if (evaluateEquation(testValueNum, numberArray)) {
+            sum += testValueNum;
+        }
+    }
+
+    return sum;
+}
+
+console.log(sumOfValidTestValues(testInput));
+console.log(sumOfValidTestValues(input));
