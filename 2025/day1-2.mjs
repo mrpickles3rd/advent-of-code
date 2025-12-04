63e8f4ec7d9bfc0eef83c071c5044301edebc334
Dan B <mrpickles3rd@googlemail.com>
2025-12-04 23:00:14 +0000

    stuff and things

--- 2025/day1-2.mjs
+++ 2025/day1-2.mjs
@@ -3,23 +3,61 @@ import { parseInput } from './_Strings.mjs';
 const ZERO = 0;
 const END = 99;
 
-function rotateDial(direction, amount, position) {
-  const newPosition = position + (direction === 'R' ? amount : -amount);
+function _calcPasses(passes, position, newPosition, amount) {
+  console.log('DB ... 🚀 ~ day1-2.mjs:7 ~ _calcPasses ~ passes, position, newPosition ~~~', passes, position, newPosition);
+  // passes: passes === 0 ? passes + 1 : position === 0 || newPosition === 0 ? passes - 1 : passes,
+  if (passes === 0 && position !== 0 && newPosition !== 0) {
+    return 1;
+  } else if (position === 0 || newPosition === 0) {
+    return passes - 1;
+  }
+  return passes;
+}
 
-  const fullRotations = Math.floor(Math.abs(newPosition + 1) / (END + 1));
+function _rotateDial(direction, amount, position) {
+  console.log('DB ... 🚀 ~ day1-2.mjs:7 ~ _rotateDial ~ direction, amount, position ~~~', `${direction}${amount}`, position);
+  const newPosition = position + (direction === 'R' ? amount : -amount);
+  console.log('DB ... 🚀 ~ day1-2.mjs:8 ~ _rotateDial ~ newPosition ~~~', newPosition);
+
+  const fullRotations = Math.floor(amount / (END + 1));
   const remainingRotations = Math.abs(newPosition + 1) % (END + 1);
+
+  // const fullRotations = Math.floor(amount / (END + 1));
+  // const remainingRotations = (newPosition) % (END + 1);
+
+  let passes = fullRotations;
+  console.log('DB ... 🚀 ~ day1-2.mjs:13 ~ _rotateDial ~ fullRotations, remainingRotations ~~~', fullRotations, remainingRotations);
 
   const LEFT = newPosition < ZERO;
   if (LEFT) {
-    return END - remainingRotations;
+    console.log('DB ... 🚀 ~ day1-2.mjs:18 ~ _rotateDial ~ LEFT ~~~', LEFT, END, remainingRotations);
+    return {
+      passes: _calcPasses(passes, position, newPosition, amount),
+      position: END - remainingRotations,
+    };
   }
 
   const RIGHT = newPosition > END;
   if (RIGHT) {
-    return remainingRotations + ZERO - 1;
+    console.log('DB ... 🚀 ~ day1-2.mjs:25 ~ _rotateDial ~ RIGHT ~~~', RIGHT);
+    return {
+      passes: _calcPasses(passes, position, newPosition, amount),
+      position: remainingRotations + ZERO - 1,
+    };
   }
 
-  return newPosition;
+  return {
+    passes,
+    position: newPosition,
+  };
+}
+
+function getPosition(direction, amount, position) {
+  return _rotateDial(direction, amount, position).position;
+}
+
+function getPasses(direction, amount, position) {
+  return _rotateDial(direction, amount, position).passes;
 }
 
 function _find(moves, startPosition) {
@@ -28,15 +66,19 @@ function _find(moves, startPosition) {
     passes: 0,
   };
   let position = startPosition;
+  let passes = 0;
 
   for (const move of moves) {
     const { direction, amount } = move;
 
-    position = rotateDial(direction, amount, position);
+    // position = getPosition(direction, amount, position);
+    ({position, passes} = _rotateDial(direction, amount, position));
+    ret.passes += passes;
 
     if (position === 0) {
       ret.zeros += 1;
     }
+    console.log('DB ... 🚀 ~ day1-2.mjs:80 ~ _find ~ ret ~~~', ret);
   }
 
   return ret;
@@ -50,4 +92,4 @@ function zerosPassed(moves, startPosition = 50) {
   return _find(moves, startPosition).passes;
 }
 
-export { rotateDial, zerosFound, zerosPassed };
+export { getPosition, getPasses, zerosFound, zerosPassed };
--- 2025/day1-2.test.mjs
+++ 2025/day1-2.test.mjs
@@ -1,45 +1,87 @@
 import { parseInput } from './_Strings.mjs';
 import { input1 } from './day1.input.mjs';
-import { rotateDial, zerosFound, zerosPassed } from './day1-2.mjs';
+import { getPosition, getPasses, zerosFound, zerosPassed } from './day1-2.mjs';
 
 const TEST_INPUT_1 = 'L68\nL30\nR48\nL5\nR60\nL55\nL1\nL99\nR14\nL82';
 
 describe('day1-2', () => {
-  describe('rotateDial', () => {
+  describe('getPosition', () => {
     it('Should rotate the dial right 5 times from 94 to 99', () => {
-      const position = rotateDial('R', 5, 94);
+      const position = getPosition('R', 5, 94);
       const lastPostion = 99;
       expect(position).toBe(lastPostion);
     });
 
     it('Should rotate the dial left 5 times from 5 to 0', () => {
-      const position = rotateDial('L', 5, 5);
+      const position = getPosition('L', 5, 5);
       const firstPosition = 0;
       expect(position).toBe(firstPosition);
     });
 
     it('Should rotate the dial right 10 times from 95 to 5', () => {
-      const position = rotateDial('R', 10, 95);
+      const position = getPosition('R', 10, 95);
       const lastPostion = 5;
       expect(position).toBe(lastPostion);
     });
 
     it('Should rotate the dial left 10 times from 5 to 95', () => {
-      const position = rotateDial('L', 10, 5);
+      const position = getPosition('L', 10, 5);
       const firstPosition = 95;
       expect(position).toBe(firstPosition);
     });
 
     it('Should rotate the dial right 200 times from 0 to 0', () => {
-      const position = rotateDial('R', 200, 0);
+      const position = getPosition('R', 200, 0);
       const firstPosition = 0;
       expect(position).toBe(firstPosition);
     });
 
     it('Should rotate the dial left 200 times from 0 to 0', () => {
-      const position = rotateDial('L', 200, 0);
+      const position = getPosition('L', 200, 0);
       const firstPosition = 0;
       expect(position).toBe(firstPosition);
+    });
+  });
+
+  describe('getPasses', () => {
+    it('Should NOT pass zero when rotating the dial right 5 times from 94 to 99', () => {
+      const passes = getPasses('R', 5, 94);
+      expect(passes).toBe(0);
+    });
+
+    it('Should NOT pass zero when rotating the dial left 5 times from 5 to 0', () => {
+      const passes = getPasses('L', 5, 5);
+      expect(passes).toBe(0);
+    });
+
+    it('Should pass zero once when rotating the dial right 10 times from 95 to 5', () => {
+      const passes = getPasses('R', 10, 95);
+      expect(passes).toBe(1);
+    });
+
+    it('Should pass zero once when rotating the dial left 10 times from 5 to 95', () => {
+      const passes = getPasses('L', 10, 5);
+      expect(passes).toBe(1);
+    });
+
+    it('Should pass zero two times when rotating the dial right 200 times from 50 to 50', () => {
+      const passes = getPasses('R', 200, 50);
+      expect(passes).toBe(2);
+    });
+
+    it('Should pass zero two times when rotating the dial left 200 times from 50 to 50', () => {
+      const passes = getPasses('L', 200, 50);
+      expect(passes).toBe(2);
+    });
+
+    it('Should pass zero once when rotating the dial right 200 times from 0 to 0', () => {
+      const passes = getPasses('R', 200, 0);
+      expect(passes).toBe(1);
+    });
+
+    it('Should pass zero once when rotating the dial left 200 times from 0 to 0', () => {
+      const passes = getPasses('L', 200, 0);
+      expect(passes).toBe(1);
     });
   });
 
@@ -64,7 +106,44 @@ describe('day1-2', () => {
   });
 
   describe('zerosPassed', () => {
-    it('Should pass 3 zeros for the test input', () => {
+
+
+/*
+      DB ... 🚀 ~ day1-2.mjs:7 ~ _rotateDial ~ direction, amount, position ~~~ L82 14
+
+      at _rotateDial (2025/day1-2.mjs:18:11)
+
+    console.log
+      DB ... 🚀 ~ day1-2.mjs:8 ~ _rotateDial ~ newPosition ~~~ -68
+
+      at _rotateDial (2025/day1-2.mjs:20:11)
+
+    console.log
+      DB ... 🚀 ~ day1-2.mjs:13 ~ _rotateDial ~ fullRotations, remainingRotations ~~~ 0 67
+
+      at _rotateDial (2025/day1-2.mjs:29:11)
+
+    console.log
+      DB ... 🚀 ~ day1-2.mjs:18 ~ _rotateDial ~ LEFT ~~~ true 99 67
+
+      at _rotateDial (2025/day1-2.mjs:33:13)
+
+    console.log
+      DB ... 🚀 ~ day1-2.mjs:7 ~ _calcPasses ~ passes, position, newPosition ~~~ 0 14 -68
+
+      at _calcPasses (2025/day1-2.mjs:7:11)
+
+    console.log
+      DB ... 🚀 ~ day1-2.mjs:80 ~ _find ~ ret ~~~ { zeros: 3, passes: 2 }
+
+      at _find (2025/day1-2.mjs:81:13)
+
+  ● day1-2 › zerosPassed › Should pass 3 zeros for the test input
+*/
+
+
+
+    it.only('Should pass 3 zeros for the test input', () => {
       const moves = parseInput(TEST_INPUT_1);
       const zeros = zerosPassed(moves, 50);
       expect(zeros).toBe(3);
